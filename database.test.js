f14d80501d3c9bd61df084e29a0c31c2a44738e1
matias.tecnosoul <matias@tecnosoul.com.ar>
2025-06-09 18:52:15 +0000

    npm run lint -- --fix

--- database.test.js
+++ database.test.js
@@ -1,4 +1,4 @@
-const { Client } = require('pg')
+const { Client } = require('pg');
 const {
   /**
    * Recuperamos el esquema esperado
@@ -6,14 +6,14 @@ const {
    * Para una primer etapa, se recomienda importar la propiedad
    * "baseFields" reenombrandola a "expectedFields"
    */
-  baseFields: expectedFields
-} = require('./schema_base')
+  baseFields: expectedFields,
+} = require('./schema_base');
 
 describe('Test database', () => {
   /**
    * Variables globales usadas por diferentes tests
    */
-  let client
+  let client;
 
   /**
    * Generamos la configuracion con la base de datos y
@@ -24,17 +24,17 @@ describe('Test database', () => {
    */
   beforeAll(async () => {
     client = new Client({
-      connectionString: process.env.DATABASE_URL
-    })
-    await client.connect()
-  })
+      connectionString: process.env.DATABASE_URL,
+    });
+    await client.connect();
+  });
 
   /**
    * Cerramos la conexion con la base de datos
    */
   afterAll(async () => {
-    await client.end()
-  })
+    await client.end();
+  });
 
   /**
    * Validamos el esquema de la base de datos
@@ -44,8 +44,8 @@ describe('Test database', () => {
      * Variable donde vamos a almacenar los campos
      * recuperados de la base de datos
      */
-    let fields
-    let result
+    let fields;
+    let result;
 
     /**
      * Generamos un objeto para simplificar el acceso en los test
@@ -62,14 +62,14 @@ describe('Test database', () => {
           information_schema.columns
         WHERE
           table_name = $1::text`,
-        ['users']
-      )
+        ['users'],
+      );
 
       fields = result.rows.reduce((acc, field) => {
-        acc[field.column_name] = field.data_type
-        return acc
-      }, {})
-    })
+        acc[field.column_name] = field.data_type;
+        return acc;
+      }, {});
+    });
 
     describe('Validate fields name', () => {
       /**
@@ -77,9 +77,9 @@ describe('Test database', () => {
        * encuentren presentes
        */
       test.each(expectedFields)('Validate field $name', ({ name }) => {
-        expect(Object.keys(fields)).toContain(name)
-      })
-    })
+        expect(Object.keys(fields)).toContain(name);
+      });
+    });
 
     describe('Validate fields type', () => {
       /**
@@ -87,59 +87,59 @@ describe('Test database', () => {
        * del tipo esperado
        */
       test.each(expectedFields)('Validate field $name to be type "$type"', ({ name, type }) => {
-        expect(fields[name]).toBe(type)
-      })
-    })
-  })
+        expect(fields[name]).toBe(type);
+      });
+    });
+  });
 
   describe('Validate insertion', () => {
     afterEach(async () => {
-      await client.query('TRUNCATE users')
-    })
+      await client.query('TRUNCATE users');
+    });
 
     test('Insert a valid user', async () => {
       let result = await client.query(
         `INSERT INTO
          users (email, username, birthdate, city)
-         VALUES ('user@example.com', 'user', '2024-01-02', 'La Plata')`
-      )
+         VALUES ('user@example.com', 'user', '2024-01-02', 'La Plata')`,
+      );
 
-      expect(result.rowCount).toBe(1)
+      expect(result.rowCount).toBe(1);
 
       result = await client.query(
-        'SELECT * FROM users'
-      )
+        'SELECT * FROM users',
+      );
 
-      const user = result.rows[0]
-      const userCreatedAt = new Date(user.created_at)
-      const currentDate = new Date()
+      const user = result.rows[0];
+      const userCreatedAt = new Date(user.created_at);
+      const currentDate = new Date();
 
-      expect(user.email).toBe('user@example.com')
-      expect(userCreatedAt.getFullYear()).toBe(currentDate.getFullYear())
-    })
+      expect(user.email).toBe('user@example.com');
+      expect(userCreatedAt.getFullYear()).toBe(currentDate.getFullYear());
+    });
 
     test('Insert a user with an invalid email', async () => {
       const query = `INSERT INTO
                      users (email, username, birthdate, city)
-                     VALUES ('user', 'user', '2024-01-02', 'La Plata')`
+                     VALUES ('user', 'user', '2024-01-02', 'La Plata')`;
 
-      await expect(client.query(query)).rejects.toThrow('users_email_check')
-    })
+      await expect(client.query(query)).rejects.toThrow('users_email_check');
+    });
 
     test('Insert a user with an invalid birthdate', async () => {
       const query = `INSERT INTO
                      users (email, username, birthdate, city)
-                     VALUES ('user@example.com', 'user', 'invalid_date', 'La Plata')`
+                     VALUES ('user@example.com', 'user', 'invalid_date', 'La Plata')`;
 
-      await expect(client.query(query)).rejects.toThrow('invalid input syntax for type date')
-    })
+      await expect(client.query(query)).rejects.toThrow('invalid input syntax for type date');
+    });
 
     test('Insert a user without city', async () => {
       const query = `INSERT INTO
                      users (email, username, birthdate)
-                     VALUES ('user@example.com', 'user', '2024-01-02')`
+                     VALUES ('user@example.com', 'user', '2024-01-02')`;
 
-      await expect(client.query(query)).rejects.toThrow('null value in column "city"')
-    })
-  })
-})
+      await expect(client.query(query)).rejects.toThrow('null value in column "city"');
+    });
+  });
+});
--- schema_base.js
+++ schema_base.js
@@ -1,55 +1,55 @@
 const baseFields = [
   {
     name: 'email',
-    type: 'character varying'
+    type: 'character varying',
   },
   {
     name: 'username',
-    type: 'character varying'
+    type: 'character varying',
   },
   {
     name: 'birthdate',
-    type: 'date'
+    type: 'date',
   },
   {
     name: 'city',
-    type: 'character varying'
+    type: 'character varying',
   },
   {
     name: 'created_at',
-    type: 'timestamp with time zone'
-  }
-]
+    type: 'timestamp with time zone',
+  },
+];
 
 const expectedFields = [
   ...baseFields,
   {
     name: 'updated_at',
-    type: 'timestamp with time zone'
+    type: 'timestamp with time zone',
   },
   {
     name: 'last_access_time',
-    type: 'timestamp with time zone'
+    type: 'timestamp with time zone',
   },
   {
     name: 'first_name',
-    type: 'character varying'
+    type: 'character varying',
   },
   {
     name: 'last_name',
-    type: 'character varying'
+    type: 'character varying',
   },
   {
     name: 'password',
-    type: 'character varying'
+    type: 'character varying',
   },
   {
     name: 'enabled',
-    type: 'boolean'
-  }
-]
+    type: 'boolean',
+  },
+];
 
 module.exports = {
   baseFields,
-  expectedFields
-}
+  expectedFields,
+};
